851b4068cfa1ad14e85b39757edd68e8c1679758
Hsposito <henrique.sposito@graduateinstitute.ch>
2020-10-16 14:28:26 +0200

    Created source document and citation for ISD dataset using use_qData() and templates from qDatr

--- data-raw/states/ISD/isd_slab.R
+++ data-raw/states/ISD/isd_slab.R
@@ -1,9 +1,9 @@
 #ISD slab
 library(qDatr)
-isd <- read_csv("data-raw/states/ISD/ISD_Version1_Dissemination.csv")
+isd <- readr::read_csv("data-raw/states/ISD/ISD_Version1_Dissemination.csv")
 #   isdslab <- as_tibble(ISD_Version1_Dissemination) %>% 
 #   transmutate(ccode = `COW Nr.`) %>% transmutate(Id = `COW ID`) %>% 
 #   transmutate(statename = `State Name`) %>% transmutate(Beg = lubridate::dmy(Start)) %>% 
 #   transmutate(End = lubridate::dmy(End)) %>%  
 #   select(-X8, -X9, -X10, -X11, -X12,  -X13,  -X14,  -X15)
-usethis::use_data(isd)
+qDatr::use_qData(isd)
