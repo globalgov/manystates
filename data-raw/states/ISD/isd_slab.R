3a5ed82675462eae8bf58a693732dd14bd00a8ef
Hsposito <henrique.sposito@graduateinstitute.ch>
2020-10-22 10:55:01 +0200

    Updated isd_slab script to reflect function name changes in qDatr as well as added more lines to replace NAs and symbols in data
    Dummy variables were imported to R as NA or X, instead of 0 and 1. This issue was fixed using dplyr functions

--- data-raw/states/ISD/isd_slab.R
+++ data-raw/states/ISD/isd_slab.R
@@ -1,9 +1,14 @@
 #ISD slab
 library(qDatr)
-isd <- readr::read_csv("data-raw/states/ISD/ISD_Version1_Dissemination.csv")
-#   isdslab <- as_tibble(ISD_Version1_Dissemination) %>% 
-#   transmutate(ccode = `COW Nr.`) %>% transmutate(Id = `COW ID`) %>% 
-#   transmutate(statename = `State Name`) %>% transmutate(Beg = lubridate::dmy(Start)) %>% 
+isddata <- readr::read_csv("data-raw/states/ISD/ISD_Version1_Dissemination.csv")
+qDatr::import_data(isddata)
+#   isd <- as_tibble(isddata) %>% 
+#   transmutate(ccode = `COW Nr.`) %>% 
+#   transmutate(Id = `COW ID`) %>% 
+#   transmutate(statename = entitle(`State Name`)) %>% 
+#   transmutate(Beg = lubridate::dmy(Start)) %>% 
 #   transmutate(End = lubridate::dmy(End)) %>%  
-#   select(-X8, -X9, -X10, -X11, -X12,  -X13,  -X14,  -X15)
-qDatr::use_qData(isd)
+#   mutate_all(funs(ifelse(is.na(.), 0, .))) %>% 
+#   mutate(across(everything(), ~replace(., . ==  "X" , 1))) %>%
+#   dplyr::select(-X8, -X9, -X10, -X11, -X12,  -X13,  -X14,  -X15)
+qDatr::export_data(isd)
