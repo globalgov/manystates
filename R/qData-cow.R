9afb9272a9591eec2565e3be00f9ade94ef61c03
BBieri <bernhard.bieri@graduateinstitute.ch>
2020-12-11 18:41:07 +0100

    Updated individual test files for included datasets
    Updated individual test files for the included datasets in qStates. These tests still work at the dataset level and not the database level at the moment.

--- R/qData-cow.R
+++ R/qData-cow.R
@@ -6,10 +6,10 @@
 #'
 #' @format This dataset contains  243 observations and 5 variables:
 #' \describe{
-#'   \item{COW_Nr}{Correlates of War numerical identifier}
-#'   \item{ID}{Letter abbreviation of the State Name}
-#'   \item{Beg}{Beginning date of State tenure}
-#'   \item{End}{End date of State tenure}
+#'   \item{COW_Nr}{Numerical COW identifier}
+#'   \item{ID}{# letter abbreviation of State name}
+#'   \item{Beg}{Beginning	date	of	state	tenure}
+#'   \item{End}{End date of state tenure}
 #'   \item{Label}{Primary COW State name}
 #' }
 #' @source \url{https://correlatesofwar.org/data-sets/state-system-membership}
