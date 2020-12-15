11bd43051590d6ff26f6bf5ef40829171d72c7cb
Hsposito <henrique.sposito@graduateinstitute.ch>
2020-12-15 11:32:48 +0100

    Updated ISD dataset preparation script and states database

--- data-raw/states/ISD/prepare-ISD.R
+++ data-raw/states/ISD/prepare-ISD.R
@@ -3,6 +3,7 @@
 # This is a template for importing, cleaning, and exporting data
 # ready for the qPackage.
 library(qData)
+library(tidyverse)
 
 # Stage one: Collecting data
 ISD <- read.csv("data-raw/states/ISD/ISD_Version1_Dissemination.csv")
@@ -11,20 +12,24 @@ ISD <- read.csv("data-raw/states/ISD/ISD_Version1_Dissemination.csv")
 # In this stage you will want to correct the variable names and
 # formats of the 'ISD' object until the object created
 # below (in stage three) passes all the tests. 
+
 ISD <- as_tibble(ISD) %>%
-  dplyr::rename(Finish = End) %>% # Renaming the end date column to avoid self reference in transmutate.
+  dplyr::rename(Finish = End) %>% 
+  # Renaming the end date column to avoid self reference in transmutate.
   transmutate(ID = `COW.ID`,
               Beg = standardise_dates(Start),
               End = standardise_dates(Finish),
-              Label = standardise_titles("State.Name"),
+              Label = standardise_titles(as.character(State.Name)),
               COW_Nr = standardise_titles(as.character(COW.Nr.))) %>%
-  dplyr::mutate_at(vars(Micro), ~replace(., .=="", 0)) %>% #  standardizes the dummies in the ISD dataset.
-  dplyr::mutate_at(vars(Micro), ~replace(., .=="X" , 1)) %>%  # replaced mutate(across(everything(), ~replace(., . ==  "X" , 1))) %>%, standardizes the dummies
-  dplyr::mutate_at(vars(New.State), ~replace(., .=="", 0)) %>% # standardizes the dummies like the previous lines, "" becomes 0 and X becomes 1
-  dplyr::mutate_at(vars(New.State), ~replace(., .=="X" , 1)) %>% # standardizes the dummies like the previous lines, "" becomes 0 and X becomes 1
-  dplyr::select(-X, -X.1, -X.2, -X.3, -X.4,  -X.5,  -X.6,  -X.7) %>% #Dropping certain unnecessary columns.
+  # Standardising the dummie variables
+  dplyr::mutate_at(vars(Micro, New.State),  ~replace(. == "", 0, .)) %>% 
+  dplyr::mutate_at(vars(Micro, New.State), ~replace(. == "x", 1, .)) %>%  
+  #Dropping certain unnecessary columns.
+  dplyr::select(-X, -X.1, -X.2, -X.3, -X.4, -X.5,  -X.6, -X.7) %>% 
+  # Arranging dataset
   dplyr::relocate(COW_Nr, ID, Beg, End, Label, Micro, New.State) %>%
   dplyr::arrange(Beg, ID)
+
 # qData includes several functions that should help cleaning and standardizing your data.
 # Please see the vignettes or website for more details.
 
