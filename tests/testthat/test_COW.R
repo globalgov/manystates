75255321bd57494837bef9b7c4394a32e6486544
BBieri <bernhard.bieri@graduateinstitute.ch>
2021-02-05 16:01:00 +0100

    Added new documentation and tests with the new version of the export_data() function

---
+++ tests/testthat/test_COW.R
@@ -0,0 +1,35 @@
+# Test if  meets the q ecosystem requirements
+
+# Requires the following package
+library(pointblank)
+
+# Report missing values 
+test_that("missing observations are reported correctly", {
+  expect_false(any(grepl("^.$", states[["COW"]])))
+  expect_false(any(grepl("^n/a$", states[["COW"]])))
+  expect_false(any(grepl("^N/A$", states[["COW"]])))
+})
+
+# Contains the required variables
+test_that("object has the correct variables", {
+  expect_col_exists(states[["COW"]], vars(ID))
+  expect_col_exists(states[["COW"]], vars(Beg))
+  expect_col_exists(states[["COW"]], vars(End))
+  expect_col_exists(states[["COW"]], vars(Label))
+})
+
+# Variables with dates are standardized
+test_that("dates are standardised", {
+  expect_col_is_date(states[["COW"]], vars(Beg))
+  expect_col_is_date(states[["COW"]], vars(End))
+  expect_false(any(grepl("/", states[["COW"]]$Beg)))
+  expect_false(any(grepl("/", states[["COW"]]$End)))
+})
+
+# Labels are standardized
+test_that("labels are standardised", {
+  expect_false(any(grepl("U.S.", states[["COW"]])))
+  expect_false(any(grepl("U.K.", states[["COW"]])))
+  expect_false(any(grepl("!", states[["COW"]])))
+  expect_false(any(grepl("NANA.", states[["COW"]])))
+})
