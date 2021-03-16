8237db7ec47e897581a29e0c5b0fcb0eca8d73ce
BBieri <bernhard.bieri@graduateinstitute.ch>
2021-03-16 18:09:51 +0100

    Added a code_modern_states() function to leverage countrycode package
    This function will serve to automatically add additional variables based on the COW_ID to modern states. This is the first version. Still needs to be amended in the future depending on the evolution of our own qID for states.

--- R/code_states.R
+++ R/code_states.R
@@ -14,10 +14,10 @@
 #' @export
 code_states <- function(v){
   
-  # countryregex <- read.csv("data-raw/Stat_Actor/stat_regex.csv", stringsAsFactors = F) %>%
+  # countryregex <- read.csv("data-raw/states/Stat_Actor/stat_regex.csv", stringsAsFactors = F) %>%
   #   as.data.frame()
   
-  # Find country codes
+  # Find country codes from the label column
   coment <- sapply(countryregex[,3], function(x) grepl(x, v, ignore.case = T, perl = T)*1)
   colnames(coment) <- countryregex[,1]
   rownames(coment) <- v
@@ -27,3 +27,41 @@ code_states <- function(v){
   out <- unname(out)
   out
 }
+
+# Additional code leveraging the already present COW_ID(ID)
+
+#' Code country specific indicators
+#'
+#' Adds various country specific codes and information to the processed 
+#' dataset. This function is only useful for contemporary countries.
+#' @param v A character vector leading to the COW_ID column
+#' @details This function uses the `countrycode` package to identify and return
+#' various country specific information such as the internet domain and unicode
+#' flags to the preprocessed dataset.
+#' @return A dataframe adding information to the pre-exported dataset.
+#' @import tibble
+#' @examples
+#' \dontrun{
+#' code_modern_states(states$ISD$ID)
+#' }
+#' @export
+code_modern_states <- function(v){
+  if("countrycode" %in% rownames(utils::installed.packages()) == FALSE){
+    stop("Please install the countrycode package before proceeding.")
+  }
+  modinfo <- suppressWarnings(
+        data.frame(v, 
+          UnicodeSymb = countrycode::countrycode(v, 'cowc', 'unicode.symbol'),
+          ISO3_ID = countrycode::countrycode(v, 'cowc', 'iso3c'),
+          EuroStat_code = countrycode::countrycode(v, 'cowc', 'eurostat'),
+          ECB_code = countrycode::countrycode(v,'cowc', 'ecb'),
+          IANA_TLD = countrycode::countrycode(v, 'cowc', "cctld"),
+          un_code = countrycode::countrycode(v, 'cowc', 'un'),
+          Continent = countrycode::countrycode(v, 'cowc', 'continent'),
+          EU = countrycode::countrycode(v, 'cowc', 'eu28'),
+          Currency = countrycode::countrycode(v, 'cowc', 'iso4217c')))
+  modinfo  <- as_tibble(modinfo)
+  modinfo
+}
+
+
