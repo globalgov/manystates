718ab6e6f44e3dd8bab51c85bbc2b3f3de052164
BBieri <bernhard.bieri@graduateinstitute.ch>
2021-07-12 11:14:09 +0200

    Re #10 Added tests for the new cshapes extraction function

---
+++ tests/testthat/test_extract_cshapes.R
@@ -0,0 +1,37 @@
+test_that("CShapes extraction returns qConsistent object", {
+  test <- import_cshapes(date = "1900-01-01")
+  expect_false(any(grepl("\\?", test)))
+  expect_false(any(grepl("^n/a$", test)))
+  expect_false(any(grepl("^N/A$", test)))
+  expect_false(any(grepl("^\\s$", test)))
+  expect_false(any(grepl("^\\.$", test)))
+  expect_false(any(grepl("N\\.A\\.$", test)))
+  expect_false(any(grepl("n\\.a\\.$", test)))
+})
+
+# Contains the required variables
+test_that("object has the correct variables", {
+  test <- import_cshapes(date = "1900-01-01")
+  expect_col_exists(test, vars(COW_Nr))
+  expect_col_exists(test, vars(Beg))
+  expect_col_exists(test, vars(End))
+  expect_col_exists(test, vars(Label))
+})
+
+# Variables with dates are standardized
+test_that("dates are standardised", {
+  test <- import_cshapes(date = "1900-01-01")
+  expect_col_is_date(test, vars(Beg))
+  expect_col_is_date(test, vars(End))
+  expect_false(any(grepl("/", test$Beg)))
+  expect_false(any(grepl("/", test$End)))
+})
+
+# Labels are standardized
+test_that("labels are standardised", {
+  test <- import_cshapes(date = "1900-01-01")
+  expect_false(any(grepl("U.S.", test)))
+  expect_false(any(grepl("U.K.", test)))
+  expect_false(any(grepl("!", test)))
+  expect_false(any(grepl("NANA.", test)))
+})
