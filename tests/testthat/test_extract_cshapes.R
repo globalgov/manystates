7ff16326c290bd8641b69d68a7dc8cc705d23800
BBieri <bernhard.bieri@graduateinstitute.ch>
2021-07-13 16:18:23 +0200

    Re #10 Added more tests for the extract_cshapes family of functions

--- tests/testthat/test_extract_cshapes.R
+++ tests/testthat/test_extract_cshapes.R
@@ -1,3 +1,4 @@
+# Tests for import_cshapes() function -----------------------------------------
 test <- import_cshapes(date = "1900-01-01")
 
 test_that("CShapes extraction returns qConsistent object", {
@@ -33,3 +34,45 @@ test_that("labels are standardised", {
   expect_false(any(grepl("!", test)))
   expect_false(any(grepl("NANA.", test)))
 })
+# Tests for import_distlist() function -----------------------------------------
+test2 <- import_distlist(date = "1900-01-01", type = "capdist")
+
+test_that("CShapes extraction returns qConsistent object", {
+  expect_false(any(grepl("\\?", test2)))
+  expect_false(any(grepl("^n/a$", test2)))
+  expect_false(any(grepl("^N/A$", test2)))
+  expect_false(any(grepl("^\\s$", test2)))
+  expect_false(any(grepl("^\\.$", test2)))
+  expect_false(any(grepl("N\\.A\\.$", test2)))
+  expect_false(any(grepl("n\\.a\\.$", test2)))
+})
+
+# Contains the required variables
+test_that("object has the correct variables", {
+  expect_col_exists(test2, vars(FromLabel))
+  expect_col_exists(test2, vars(ToLabel))
+  expect_col_exists(test2, vars(FromCode))
+  expect_col_exists(test2, vars(ToCode))
+  expect_col_exists(test2, vars(Distance))
+})
+
+# Variables with dates are standardized
+test_that("Distances are standardised", {
+  expect_true(is.numeric(test2$Distance))
+})
+
+# Labels are standardized
+test_that("labels are standardised", {
+  expect_false(any(grepl("U.S.", test2)))
+  expect_false(any(grepl("U.K.", test2)))
+  expect_false(any(grepl("!", test2)))
+  expect_false(any(grepl("NANA.", test2)))
+})
+
+# Tests for import_cshapes() function -----------------------------------------
+test3 <- import_distmatrix(date = "1900-01-01", type = "capdist")
+
+test_that("Matrix has the correct format", {
+  expect_true(is.matrix(test3))
+})
+
