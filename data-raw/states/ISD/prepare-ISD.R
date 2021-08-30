335c74aded5b60f3a5af14cd44df1874923a01c7
Hsposito <henrique.sposito@graduateinstitute.ch>
2021-08-30 15:25:12 +0200

    Updated preparation scripts for COW, ISD, and GW datasets to use new annotate functions in messydates package. The datasets were re-exported to states database.

--- data-raw/states/ISD/prepare-ISD.R
+++ data-raw/states/ISD/prepare-ISD.R
@@ -26,18 +26,14 @@ ISD <- tibble::as_tibble(ISD) %>%
   # Arranging dataset
   dplyr::relocate(ID, Beg, End, COW_Nr, Label, Micro, New.State) %>%
   dplyr::arrange(Beg, ID)
-# We know that COW data for "old" states is set to 1816-01-01 by default.
+# We know that ISD uses COW data for "old" states that is set to 1816-01-01 by default.
 # This is a rather uncretain date, that is, the dataset considers them states
 # on 1st January 1816, but they may have been established (much) earlier.
-# Let's signal to this uncretainty using `standardise_dates()` is a wrapper
-# for the `{messydates}` package which is designed to deal with date uncretianty.
-ISD$Beg <- qCreate::standardise_dates(stringr::str_replace_all(ISD$Beg,
-                                                               "1816-1-1|1816-01-1|1816-1-01|1816-01-01",
-                                                               "..1816-01-01"))
-# We can do the same for End dates to signal uncertainty. 
-ISD$End <- qCreate::standardise_dates(stringr::str_replace_all(ISD$End,
-                                                               "2011-12-31",
-                                                               "2011-12-31.."))
+# Let's signal to this uncretainty using the `{messydates}` package,
+# which is designed to deal with date uncertainty.
+ISD <- ISD %>% dplyr::mutate(Beg = messydates::on_or_before(ISD, "Beg", "1816-01-01"),
+                             End = messydates::on_or_after(ISD, "End", "2011-12-31"))
+# qData and qCreate include several other
 # qData and qCreate include several other
 # functions that should help cleaning and
 # standardizing your data.
