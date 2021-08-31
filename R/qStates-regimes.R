414c7431157e70e2b5f30aeb58efdbd24cd2cc1e
BBieri <bernhard.bieri@graduateinstitute.ch>
2021-08-31 09:31:59 +0200

    Updated Polity5 documenation

--- R/qStates-regimes.R
+++ R/qStates-regimes.R
@@ -3,14 +3,10 @@
 #' @format The regimes database is a list that contains the
 #' following 1 datasets: Polity5.
 #' For more information and references to each of the datasets used,
-#' please use the `data_source()` and `data_contrast()`.
+#' please use the `data_source()`, `data_contrast()`, and `data_evolution()`
 #' functions.
-#' \describe{
-#' \item{Polity5: }{A dataset with 17574 observations and the
-#' following 32 variables: ID, Beg, End, Label, p5, cyear,
-#' year, flag, fragment, democ, autoc, polity, polity2,
-#' durable, xrreg, xrcomp, xropen, xconst, parreg,
-#' parcomp, exrec, exconst, polcomp, prior, eprec,
-#' interim, bprec, post, change, d5, sf, regtrans.}
+#'\describe{
+#' \item{Polity5: }{A dataset with 17574 observations and the following
+#' 33 variables: ID, year, Label, Beg, End, p5, cyear, flag, fragment, democ, autoc, polity, polity2, durable, xrreg, xrcomp, xropen, xconst, parreg, parcomp, exrec, exconst, polcomp, prior, eprec, interim, bprec, post, change, d5, sf, regtrans, speccat.}
 #' }
-"regimes"
+ "regimes"
