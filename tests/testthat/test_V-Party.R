9e3a43ad1f4f7fa04374c52e2108ab245a302358
BBieri <bernhard.bieri@graduateinstitute.ch>
2021-10-04 18:24:17 +0200

    Re #22 and # 23 Added tests for import_vdem() and import_vparty()

--- tests/testthat/test_V-Party.R
+++ tests/testthat/test_V-Party.R
@@ -1,18 +1,42 @@
-# Test if  meets the q ecosystem requirements
+# Test if V-Party meets the qVerse requirements
 
-# Report missing values
+# Import v-Party
+vparty <- import_vparty()
+
+# Report missing values on a random subset of vparty
 test_that("missing observations are reported correctly", {
-  expect_false(any(grepl("^n/a$", regimes[["V-Party"]])))
-  expect_false(any(grepl("^N/A$", regimes[["V-Party"]])))
-  expect_false(any(grepl("^\\s$", regimes[["V-Party"]])))
-  expect_false(any(grepl("^\\.$", regimes[["V-Party"]])))
-  expect_false(any(grepl("N\\.A\\.$", regimes[["V-Party"]])))
-  expect_false(any(grepl("n\\.a\\.$", regimes[["V-Party"]])))
+  expect_false(any(grepl("^n/a$", vparty[sample(nrow(vparty), 1000), ])))
+  expect_false(any(grepl("^N/A$", vparty[sample(nrow(vparty), 1000), ])))
+  expect_false(any(grepl("^\\s$", vparty[sample(nrow(vparty), 1000), ])))
+  expect_false(any(grepl("^\\.$", vparty[sample(nrow(vparty), 1000), ])))
+  expect_false(any(grepl("N\\.A\\.$", vparty[sample(nrow(vparty), 1000), ])))
+  expect_false(any(grepl("n\\.a\\.$", vparty[sample(nrow(vparty), 1000), ])))
 })
 
 # Date columns should be in messydt class
 test_that("Columns are not in date, POSIXct or POSIXlt class", {
-  expect_false(any(lubridate::is.Date(regimes[["V-Party"]])))
-  expect_false(any(lubridate::is.POSIXct(regimes[["V-Party"]])))
-  expect_false(any(lubridate::is.POSIXlt(regimes[["V-Party"]])))
+  expect_false(any(lubridate::is.Date(vparty)))
+  expect_false(any(lubridate::is.POSIXct(vparty)))
+  expect_false(any(lubridate::is.POSIXlt(vparty)))
+})
+
+test_that("Columns with dates are standardized", {
+  expect_equal(class(vparty$Beg), "messydt")
+  expect_false(any(grepl("/", vparty$Beg)))
+  expect_false(any(grepl("^[:alpha:]$",
+                         vparty$Beg)))
+  expect_false(any(grepl("^[:digit:]{2}$",
+                         vparty$Beg)))
+  expect_false(any(grepl("^[:digit:]{3}$",
+                         vparty$Beg)))
+  expect_false(any(grepl("^[:digit:]{1}$",
+                         vparty$Beg)))
+})
+
+# Contains the required variables
+test_that("object has the correct variables", {
+  expect_col_exists(vparty, vars(ID))
+  expect_col_exists(vparty, vars(Beg))
+  expect_col_exists(vparty, vars(End))
+  expect_col_exists(vparty, vars(Label))
 })
