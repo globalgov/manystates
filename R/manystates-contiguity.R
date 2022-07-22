5dbbe614b1543b37b708e2746a71393c4658797b
Jael Tan <jael.tan@graduateinstitute.ch>
2022-07-22 11:09:48 +0200

    Imported GNEVAR contiguity and FAO membership data

--- R/manystates-contiguity.R
+++ R/manystates-contiguity.R
@@ -1,12 +1,15 @@
 #' contiguity database documentation
 #'
 #' @format The contiguity database is a list that contains the
-#' following 1 datasets: DIRCONT.
+#' following 2 datasets: DIRCONT, GNEVAR_CONT.
 #' For more information and references to each of the datasets used,
 #' please use the `data_source()` and `data_contrast()` functions.
 #'\describe{
 #' \item{DIRCONT: }{A dataset with 847 observations and the following
 #' 8 variables: dyadID, ContiguityType, Beg, End, stateID1, Label1, stateID2, Label2.}
+#' \item{GNEVAR_CONT: }{A dataset with 192 observations and the following
+#' 9 variables: stateID, Label, Beg, End, Contiguity, EntityType, FAOmembership,
+#' Group, url.}
 #' }
 #'
 #' @details
