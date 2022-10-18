c25e01d00b48ab61020abe115cba302c8de44339
Tan Yi Jael <jael.tan@graduateinstitute.ch>
2022-10-18 11:56:24 +0200

    replaced GNEVAR_ARCHIGOS with HUGGO_ARCHIGOS

--- R/manystates-leaders.R
+++ R/manystates-leaders.R
@@ -1,15 +1,10 @@
 #' leaders database documentation
 #'
-#' @import manydata
 #' @format The leaders database is a list that contains the
-#' following 2 datasets: ARCHIGOS, GNEVAR_ARCHIGOS.
-#' GNEVAR_* datasets are handcoded original data or
-#' corrections to existing datasets where coding errors were found.
-#' In the latter case, the suffix will reference the original dataset.
-#' In the leaders database, GNEVAR_ARCHIGOS contains corrections to
-#' birth dates and familial ties of states' leaders.
+#' following 2 datasets: ARCHIGOS, HUGGO_ARCHIGOS.
 #' For more information and references to each of the datasets used,
 #' please use the `data_source()` and `data_contrast()` functions.
+#' @imports manydata
 #'\describe{
 #' \item{ARCHIGOS: }{A dataset with 17686 observations and the following
 #' 30 variables: archigosID, leaderID, cowID, idacr, Label, leader, Beg, End,
@@ -17,7 +12,7 @@
 #' prevtimesinoffice, posttenurefate, dbpedia.uri, num.entry, num.exit,
 #' num.exitcode, num.posttenurefate, FtiesNameA, FtiesCodeA, FtiesNameB,
 #' FtiesCodeB, FtiesNameC, FtiesCodeC, ftcur.}
-#' \item{GNEVAR_ARCHIGOS: }{A dataset with 24 observations and the following
+#' \item{HUGGO_ARCHIGOS: }{A dataset with 24 observations and the following
 #' 30 variables: archigosID, cowID, leaderID, idacr, Label, leader, Beg, End,
 #' BornDate, DeathDate, YearBorn, YearDied, Female, entry, exit, exitcode,
 #' prevtimesinoffice, posttenurefate, dbpedia.uri, num.entry, num.exit,
