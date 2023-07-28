64abea9251d57c8eb42ddbab4ee0bcdd140f4ed0
Henrique Sposito <henrique.sposito@graduateinstitute.ch>
2023-07-28 17:00:46 +0200

    Added package startup message

--- R/zzz.R
+++ R/zzz.R
@@ -1,6 +1,6 @@
 .onAttach <- function(lib, pkg) {
   msg <- c(paste0("manystates ", utils::packageVersion("manystates")),
-           "\nFor more information about the package pleae visit https://globalgov.github.io/manystates/",
+           "\nFor more information about the package please visit https://globalgov.github.io/manystates/",
            "\nType 'citation(\"manystates\")' for citing this R package in publications.")
   packageStartupMessage(msg)      
   invisible()
