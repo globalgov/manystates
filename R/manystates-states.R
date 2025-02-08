d1eb292ef4356ca45694d6869e744562a3828bb0
James Hollway <james.hollway@graduateinstitute.ch>
2025-02-08 17:50:57 +0100

    Dropped economic freedom dataset

--- R/manystates-states.R
+++ R/manystates-states.R
@@ -1,8 +1,7 @@
 #' states datacube documentation
 #'
 #' @format The states datacube is a list that contains the
-#' following 8 datasets: GW, ISD, COW, ICOW, RATRULES, HUGGO_STATES,
-#' EconomicFreedom, EconomicFreedomHist.
+#' following 8 datasets: GW, ISD, COW, ICOW, RATRULES, and HUGGO_STATES.
 #' For more information and references to each of the datasets used,
 #' please use the `manydata::call_sources()` and `manydata::compare_dimensions()` functions.
 #'\describe{
@@ -21,38 +20,6 @@
 #' 14 variables: stateID, StateName, Capital, Begin, End, StateName2, Capital2,
 #' Latitude, Longitude, Area, Region, RatProcedure, Constitutional Description,
 #' Source_rat.}
-#' \item{EconomicFreedom: }{A dataset with 4290 observations and the following
-#' 73 variables: cowID, Year, StateName, Economic Freedom Summary Index, Rank,
-#' Quartile, 1A Government Consumption, 1A_data, 1B  Transfers and subsidies,
-#' 1B_data, 1C  Government investment, 1C_data, 1Di Top marginal income tax rate,
-#' 1Di_data, 1Dii Top marginal income and payroll tax rate, 1Dii_data,
-#' 1D  Top marginal tax rate, IE State Ownership of Assets, 1  Size of Government,
-#' 2A  Judicial independence, 2B  Impartial courts, 2C  Protection of property rights,
-#' 2D  Military interference in rule of law and politics,
-#' 2E Integrity of the legal system, 2F Legal enforcement of contracts,
-#' 2G Regulatory restrictions on the sale of real property,
-#' 2H Reliability of police, Gender Legal Rights Adjustment,
-#' 2  Legal System & Property Rights, 3A  Money growth, 3A_data,
-#' 3B  Standard deviation of inflation, 3B_data, 3C  Inflation: Most recent year,
-#' 3C_data, 3D  Freedom to own foreign currency bank accounts, 3  Sound Money,
-#' 4Ai  Revenue from trade taxes (% of trade sector), 4Ai_data,
-#' 4Aii  Mean tariff rate, 4Aii_data, 4Aiii  Standard deviation of tariff rates,
-#' 4Aiii_data, 4A  Tariffs, 4Bi  Non-tariff trade barriers,
-#' 4Bii  Compliance costs of importing and exporting, 4B  Regulatory trade barriers,
-#' 4C  Black market exchange rates, 4Di  Financial Openness, 4Dii  Capital controls,
-#' 4Diii Freedom of foreigners to visit, 4D  Controls of the movement of capital and people,
-#' 4  Freedom to trade internationally, 5Ai  Ownership of banks, 5Aii Private sector credit,
-#' 5Aiii  Interest rate controls/negative real interest rates),
-#' 5A  Credit market regulations, 5Bi  Hiring regulations and minimum wage,
-#' 5Bii  Hiring and firing regulations, 5Biii  Centralized collective bargaining,
-#' 5Biv  Hours Regulations, 5Bv Mandated cost of worker dismissal, 5Bvi  Conscription,
-#' 5B  Labor market regulations, 5Ci  Administrative requirements, 5Cii  Regulatory Burden,
-#' 5Ciii  Starting a  business, 5Civ  Impartial Public Administration,
-#' 5Cv Licensing restrictions, 5Cvi Tax compliance, 5C  Business regulations, 
-#' 5  Regulation, stateID.}
-#' \item{EconomicFreedomHist: }{A dataset with 458 observations and the following
-#' 5 variables: stateID, Year, StateName, EFW, Rank.}
-#' }
 #' @source
 #' \itemize{
 #' \item{GW: }{
@@ -159,37 +126,6 @@
 #' |:------------:|:------------:|
 #' | StatID | stateID |
 #' | Rat | RatProcedure |
-#' 
-#' }
-#' \item{EconomicFreedom: }{
-#' Variable Mapping
-#'
-#' |  *from*  | *to*
-#' |:------------:|:------------:|
-#' | Countries | StateName |
-#' | ISO_Code_3 | cowID |
-#' | data...9 | 1A_data |
-#' | data...11 | 1B_data |
-#' | data...13 | 1C_data |
-#' | data...15 | 1Di_data |
-#' | data...17 | 1Dii_data |
-#' | data...32 | 3A_data |
-#' | data...34 | 3B_data |
-#' | data...36 | 3C_data |
-#' | data...40 | 4Ai_data |
-#' | data...42 | 4Aii_data |
-#' | data...44 | 4Aiii_data |
-#' 
-#' }
-#' \item{EconomicFreedomHist: }{
-#' Variable Mapping
-#'
-#' |  *from*  | *to*
-#' |:------------:|:------------:|
-#' | Country | StateName |
-#' 
-#' }
-#' }
 #' @md
 #' @details
 #' ``` {r, echo = FALSE, warning = FALSE}
