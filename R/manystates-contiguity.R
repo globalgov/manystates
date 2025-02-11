9895aa2f24df42ea5b2f2c7fe41986796767d00e
James Hollway <james.hollway@graduateinstitute.ch>
2025-02-11 13:14:14 +0100

    Updated contiguity documentation with complete citation

--- R/manystates-contiguity.R
+++ R/manystates-contiguity.R
@@ -1,33 +1,30 @@
 #' State contiguity datacube
 #'
-#' @description The contiguity datacube is a list containing 
+#' @description The `contiguity` datacube is a list containing 
 #' `r length(manystates::contiguity)` datasets: 
 #' `r cli::pluralize("{names(manystates::contiguity)}")`.
-#' It is a work-in-progress, so please do not rely on it yet.
+#' It is a work-in-progress, so please let us know if you have any comments or suggestions.
 #' @format
 #' \describe{
-#' \item{COW: }{A dataset with `r prettyNum(nrow(manystates::contiguity$COW), big.mark=",")` 
-#' observations and `r ncol(manystates::contiguity$COW)` variables: 
-#' `r cli::pluralize("{names(manystates::contiguity$COW)}")`.}
 #' \item{HUGGO: }{A dataset with `r prettyNum(nrow(manystates::contiguity$HUGGO), big.mark=",")` 
 #' observations and `r ncol(manystates::contiguity$HUGGO)` variables: 
 #' `r cli::pluralize("{names(manystates::contiguity$HUGGO)}")`.}
+#' \item{COW: }{A dataset with `r prettyNum(nrow(manystates::contiguity$COW), big.mark=",")` 
+#' observations and `r ncol(manystates::contiguity$COW)` variables: 
+#' `r cli::pluralize("{names(manystates::contiguity$COW)}")`.}
 #' }
 #' For more information and references to each of the datasets used,
 #' please use the `manydata::call_sources()` and `manydata::compare_dimensions()` functions.
 #' @source
-#'\itemize{
+#' \describe{
 #' \item{COW: }{
 #' Hensel, Paul R. _Correlates of War Project. Direct Contiguity Data, 1816-2016. Version 3.2._ 2017.
 #' \url{https://correlatesofwar.org/data-sets/direct-contiguity}
 #' }
 #' \item{HUGGO: }{
-#' J. Hollway. manystates: States for manydata. 2021.}
+#' Hollway, James, Henrique Sposito, and Jael Tan. 2001. _manystates: States for manydata_.}
 #' }
 #' @section Mapping:
-#'\itemize{
-#' \item{COW_CONT: }{
-#' Variable Mapping
 #'
 #' |  *manystates*  | *COW*
 #' |:------------|:------------|
@@ -39,9 +36,6 @@
 #' | cowNR1 | statelno |
 #' | cowNR2 | statehno |
 #' | ContiguityType | conttype |
-#' 
-#' }
-#' }
 #' @md
 #' @details
 #' ``` {r, echo = FALSE, warning = FALSE}
