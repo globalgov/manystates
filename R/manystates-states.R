37513df3a14867f9f62855f6fdcca76a62a64102
Jael Tan <jael.tan@graduateinstitute.ch>
2025-05-26 10:51:28 +0200

    Re-rendered documentation for states datacube

--- R/manystates-states.R
+++ R/manystates-states.R
@@ -3,15 +3,15 @@
 #'   It is a work-in-progress, so please let us know if you have any comments or suggestions.
 #' @format
 #' \describe{
+#' \item{ISD: }{A dataset with `r prettyNum(nrow(manystates::states$ISD), big.mark=",")` 
+#' observations and `r ncol(manystates::states$ISD)` variables: 
+#' `r cli::pluralize("{names(manystates::states$ISD)}")`.}
 #' \item{HUGGO: }{A dataset with `r prettyNum(nrow(manystates::states$HUGGO), big.mark=",")` 
 #' observations and `r ncol(manystates::states$HUGGO)` variables: 
 #' `r cli::pluralize("{names(manystates::states$HUGGO)}")`.}
 #' \item{GW: }{A dataset with `r prettyNum(nrow(manystates::states$GW), big.mark=",")` 
 #' observations and `r ncol(manystates::states$GW)` variables: 
 #' `r cli::pluralize("{names(manystates::states$GW)}")`.}
-#' \item{ISD: }{A dataset with `r prettyNum(nrow(manystates::states$ISD), big.mark=",")` 
-#' observations and `r ncol(manystates::states$ISD)` variables: 
-#' `r cli::pluralize("{names(manystates::states$ISD)}")`.}
 #' }
 #' For more information and references to each of the datasets used,
 #' please use the `manydata::call_sources()` and `manydata::compare_dimensions()` functions.
@@ -36,10 +36,10 @@
 "states"
 
 info_states <- tibble::tibble(Dataset = names(data(states, package = "manystates")),
-                               Source = c("Hollway, James, Henrique Sposito, and Jael Tan. 2021. States for manydata.",
-                                          "Gleditsch, K.S., and M. D. Ward. 'Interstate system membership: A revised list of the independent states since 1816'. International Interactions 25.4 (1999), pp. 393-413.",
-                                          "Griffiths, R.D., and C. R. Butcher. 'Introducing the international system(s) dataset (ISD), 1816-2011'. International Interactions 39.5 (2013), pp. 748-768."),
-                               URL = c("",
-                                       "http://ksgleditsch.com/data-4.html",
-                                       "http://www.ryan-griffiths.com/data"))
+                               Source = c("Griffiths, R.D., and C. R. Butcher. 'Introducing the international system(s) dataset (ISD), 1816-2011'. International Interactions 39.5 (2013), pp. 748-768.",
+                                          "Hollway, James, Henrique Sposito, and Jael Tan. 2021. States for manydata.",
+                                          "Gleditsch, K.S., and M. D. Ward. 'Interstate system membership: A revised list of the independent states since 1816'. International Interactions 25.4 (1999), pp. 393-413."),
+                               URL = c("http://www.ryan-griffiths.com/data",
+                                       "",
+                                       "http://ksgleditsch.com/data-4.html"))
 
