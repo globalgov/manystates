fb9d2dd6c18e2d0549f40cfb3a0e259a02523fe9
James Hollway <james.hollway@graduateinstitute.ch>
2025-05-30 14:12:52 +0200

    Avoiding dplyr dependency in tests

--- tests/testthat/test_code_states.R
+++ tests/testthat/test_code_states.R
@@ -1,9 +1,8 @@
 states_test <- read.csv(paste0(testthat::test_path(), "/ctry_orig2ctry_pref.csv"),
                         encoding = "UTF-8")
-states_test <- dplyr::as_tibble(states_test)
-states_test <- dplyr::filter(states_test, 
-                             nchar(ctry_orig) > 2)
+states_test <- subset(states_test, nchar(ctry_orig) > 2)
 test_that("code_states works", {
   expect_equal(sum(is.na(code_states(states_test$ctry_orig))), 0)
 })
+# states_test <- dplyr::as_tibble(states_test)
 # states_test[is.na(code_states(states_test$ctry_orig)),] %>% print(n = 30)
