f20dd28d46cf72c89a227f5bebf7d3eb3c66a31d
James Hollway <james.hollway@graduateinstitute.ch>
2025-05-30 14:03:34 +0200

    Updated test_code_states to check that there are no missing codes, TODO check code assignment

--- tests/testthat/test_code_states.R
+++ tests/testthat/test_code_states.R
@@ -1,5 +1,9 @@
-states_test <- read.csv(paste0(testthat::test_path(), "/ctry_orig2ctry_pref.csv"))
+states_test <- read.csv(paste0(testthat::test_path(), "/ctry_orig2ctry_pref.csv"),
+                        encoding = "UTF-8")
 states_test <- dplyr::as_tibble(states_test)
 states_test <- dplyr::filter(states_test, 
                              nchar(ctry_orig) > 2)
-states_test[is.na(code_states2(states_test$ctry_orig)),] %>% print(n = 30)
+test_that("code_states works", {
+  expect_equal(sum(is.na(code_states(states_test$ctry_orig))), 0)
+})
+# states_test[is.na(code_states(states_test$ctry_orig)),] %>% print(n = 30)
